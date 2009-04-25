9687dd26304dcc8d16d217121acf40db82c68b51
U-koki-PC\koki <koki@koki-PC.(none)>
2009-04-25 21:22:38 +0900

    Code reduced a litle.

--- lib/arduino_io.rb
+++ lib/arduino_io.rb
@@ -1,14 +1,11 @@
 require 'lib/wincom'
 
-class ArduinoIO < Serial
+class ArduinoIO
   def initialize(port_no)
     @io = Serial.new
     @port_no = port_no
-    self.open
-    @io
-  end
-  def open
     @io.open(@port_no,0x0000,115200,8,0,0,256,256)
+    @io
   end
   def read
     @io.receive
