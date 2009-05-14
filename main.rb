8fb6dd6264a7d6e0bd5a9c58c3e8ccdb92282515
Koki Hashimoto <gohannnotomo@gmail.com>
2009-05-14 11:41:38 +0900

    Made to show ist of notes and drums. see: http://github.com/koki-h/ele_drum/issues/#issue/1

--- main.rb
+++ main.rb
@@ -1,7 +1,24 @@
+require 'optparse'
+
 require 'lib/winMidiIF'
 require 'lib/arduino_io'
 require 'lib/ele_drum'
 require 'lib/song_pattern'
+require 'lib/help.rb'
+
+ARGV.options {|opt|
+    #音色・音階の名前リストを表示する
+    opt.on('--drums') do
+      Help::print_drums
+      exit 0
+    end
+    opt.on('--notes') do
+      Help::print_notes
+      exit 0
+    end
+
+    opt.parse!
+}
 
 output = MidiDevice.new
 input = ArduinoIO.new(6)
