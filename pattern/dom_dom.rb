081b0294dd3f695c14fe9a35955fd1c91f6e034c
Koki Hashimoto <gohannnotomo@gmail.com>
2009-06-07 17:58:38 +0900

    Added a pattern file. It sounds like a club "dom dom" music.

---
+++ pattern/dom_dom.rb
@@ -0,0 +1,7 @@
+drum_channel = device.openChannel(9)
+if (count % 2 == 0)
+  drum_channel.playMidiSound(BassDrum2, velo)
+end
+if (count % 4 == 3)
+  drum_channel.playMidiSound(ClosedHiHat, velo)
+end
