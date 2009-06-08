f3972e63ac7da6747a3958ebd36259849a198dd4
Koki Hashimoto <gohannnotomo@gmail.com>
2009-06-08 11:18:57 +0900

    Modefied "dom_dom" pattern file a littele. I like current version of this.

--- pattern/dom_dom.rb
+++ pattern/dom_dom.rb
@@ -2,6 +2,6 @@ drum_channel = device.openChannel(9)
 if (count % 2 == 0)
   drum_channel.playMidiSound(BassDrum2, velo)
 end
-if (count % 4 == 3)
+if (count % 8 == 3)
   drum_channel.playMidiSound(ClosedHiHat, velo)
 end
